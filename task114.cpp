c265d609c1a66d6199fb0b80620f7e69bc838c4d
O'tkirbek Yo'ldoshev <91976877+UtkirbekYuldoshev96@users.noreply.github.com>
2022-11-08 22:26:42 +0500

    Create task114.cpp

---
+++ task114.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cout<<"A qiymat kiriting: ";
+    cin>>a;
+    cout<<"B qiymat kiriting: ";
+    cin>>b;
+    cout<<"C qiymat kiriting: ";
+    cin>>c;
+
+
+    if(a>0){
+        cout<<"Mart katta "<<a<<endl;
+    }else{
+        cout<<"A Manfiy son kiritingiz: "<<endl;
+    }
+
+    if(b>0){
+        cout<<"Necha mart katta "<<b<<endl;
+    }else{
+        cout<<"B Manfiy son kiritingiz: "<<endl;
+    }
+
+    if(c>0){
+        cout<<"Necha mart katta "<<c<<endl;
+    }else{
+        cout<<"C Manfiy son kiritingiz: "<<endl;
+    }
+}
