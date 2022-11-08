6cae394a586cd79426833bf2415234cc9c70cc53
O'tkirbek Yo'ldoshev <91976877+UtkirbekYuldoshev96@users.noreply.github.com>
2022-11-08 22:13:14 +0500

    Create task2.cpp

---
+++ task2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // S=a^2 toping
+    int a;
+    cout<< "Son kiriting: ";
+    cin>> a;
+
+    a=a*a;
+    cout<< "Yuzasi: "<< a;
+    return 0;
+}
