a2847afcf1880444d4000b2a85b3c2c982baf4f2
O'tkirbek Yo'ldoshev <91976877+UtkirbekYuldoshev96@users.noreply.github.com>
2022-11-08 22:07:54 +0500

    Create task1.cpp

---
+++ task1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // formulani yeching P=4*a
+    int a,b;
+    cout<< "tomonlarni kiriting: ";
+    cin>> a;
+    b=a*4;
+    cout<< "Peremetr: "<< b;
+    return 0;
+}
