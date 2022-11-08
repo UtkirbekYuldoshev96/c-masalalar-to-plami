bfe444b0bf9503b29339cc09d7f52367ca76d57d
O'tkirbek Yo'ldoshev <91976877+UtkirbekYuldoshev96@users.noreply.github.com>
2022-11-08 22:43:38 +0500

    Create task160.cpp

---
+++ task160.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int M;
+    cout<<"M="<<M;
+    cin>>M;
+
+    for(int i=0; i<=M; i++){
+        cout<<"i="<<i<<" son ketmaligi="<<M+i<<endl;
+    }
+    return 0;
+}
