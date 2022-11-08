2731bcb414a5463628da8ae4fd47a0ceaf9eb7af
O'tkirbek Yo'ldoshev <91976877+UtkirbekYuldoshev96@users.noreply.github.com>
2022-11-08 22:45:44 +0500

    Create task161.cpp

---
+++ task161.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    float i,a,b;
+    cout<<"a=";
+    cin>>a;
+    cout<<"b=";
+    cin>>b;
+
+    for(i=a; i<=b; i++){
+        cout<<"a katta b gacha ==>"<<i+b<<endl;
+    }
+    return 0;
+}
