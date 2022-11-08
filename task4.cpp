a9132e6b9cc56c337cb306ee56b1310a9bca4e42
O'tkirbek Yo'ldoshev <91976877+UtkirbekYuldoshev96@users.noreply.github.com>
2022-11-08 22:15:52 +0500

    Aylananing uzunligini topish

---
+++ task4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   // L=p*d
+    int a, L;
+    double PI=3.14;
+
+    cout<< "A son: ";
+    cin>> a;
+
+    L=a*PI;
+
+    cout<<"Aylananing uzunligi "<< L;
+    return 0;
+}
